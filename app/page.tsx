3b64787617bc4b34484da10f47ff2fc45ef16821
Ricardo Rivera <ingrrivera@gmail.com>
2026-02-17 20:59:26 -0300

    Fix homepage render

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 export default function HomePage() {
-  return null
+  return (
+    <main style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "Arial"
+    }}>
+      <h1>Lisa está viva 🚀</h1>
+    </main>
+  )
 }
-
