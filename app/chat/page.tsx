01a121b5e3d679df18d126875b03df4160dcd57a
Ricardo Rivera <ingrrivera@gmail.com>
2026-03-22 17:21:49 -0300

    fix page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,5 +1,15 @@
-import ChatPage from "./ChatPage";
+"use client";
+
+import ChatPage from "./chat/ChatPage";
 
 export default function Page() {
-  return <ChatPage />;
+  return (
+    <>
+      <div style={{ padding: 40 }}>
+        <h1>Buscador de Prospectos Industriales</h1>
+      </div>
+
+      <ChatPage />
+    </>
+  );
 }
