478509152d17889796099440116def28c4ab4784
Ricardo Rivera <ingrrivera@gmail.com>
2026-03-22 19:20:44 -0300

    fix root chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,15 +1,5 @@
-"use client";
+import ChatUI from "./ChatUI";
 
-import ChatPage from "./chat/ChatPage";
-
-export default function Page() {
-  return (
-    <>
-      <div style={{ padding: 40 }}>
-        <h1>Buscador de Prospectos Industriales</h1>
-      </div>
-
-      <ChatPage />
-    </>
-  );
+export default function Home() {
+  return <ChatUI />;
 }
